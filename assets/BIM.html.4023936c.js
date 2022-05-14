20ff2edd0653695dd975d9d52ffcddcaad955a15
jiange1236 <183465355@qq.com>
2022-05-14 10:47:22 +0000

    Deploying to gh-pages from @ jiange1236/jiange1236.GitHub.io@1625e977fc6ebcb04ace8d30d1a1cb91474c62d0 🚀

---
+++ assets/BIM.html.4023936c.js
@@ -0,0 +1,1 @@
+const e={key:"v-e37972d0",path:"/work/BIM.html",title:"",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[],git:{createdTime:1652507372e3,updatedTime:1652507372e3,contributors:[{name:"jiange1236",email:"183465355@qq.com",commits:3}]},readingTime:{minutes:.29,words:88},filePathRelative:"work/BIM.md"};export{e as data};
