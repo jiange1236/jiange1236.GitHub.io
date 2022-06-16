c20807610b0676152889f9fcc7a01a8c99933b67
jiange1236 <183465355@qq.com>
2022-06-16 15:07:31 +0800

    markdown

--- notes/.vuepress/config.ts
+++ notes/.vuepress/config.ts
@@ -3,10 +3,6 @@ import theme from "./themeConfig";
 import docsearchPlugin from "@vuepress/plugin-docsearch";
 
 export default defineUserConfig({
-  lang: "zh-CN",
-  title: "Zeblog",
-  description: "个人学习的笔记，记录学习过程遇到的问题，学到的知识，收集各种学习工具，各种技巧，各种使用教程。",
-
   base: "/",
 
   dest: "./dist",
@@ -25,6 +21,7 @@ export default defineUserConfig({
       })();
       `
     ],
+	
 
     // 解决403错误
     ["meta", { name: "referrer", content: "no-referrer" }],
@@ -43,8 +40,17 @@ export default defineUserConfig({
       description: "个人学习的笔记，记录学习过程遇到的问题，学到的知识，收集各种学习工具，各种技巧，各种使用教程。",
     },
   },
+  
+  markdown: {
+    extractHeaders: {
+	  level: [1,3],
+	},
+  },
+  
   theme,
+  
   shouldPrefetch: false,
+  
   plugins: [
     docsearchPlugin({
       appId: "2T68NYRRBQ",
