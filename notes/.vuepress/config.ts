85790de3425cefabfee13f239035772113e1be6d
jiange1236 <183465355@qq.com>
2022-06-16 14:22:39 +0800

    yarn

--- notes/.vuepress/config.ts
+++ notes/.vuepress/config.ts
@@ -1,6 +1,6 @@
 import { defineUserConfig } from "vuepress";
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
-import hopeTheme from "./themeConfig";
+import theme from "./theme";
+import docsearchPlugin from "@vuepress/plugin-docsearch";
 
 export default defineUserConfig({
   lang: "zh-CN",
@@ -25,10 +25,9 @@ export default defineUserConfig({
       })();
       `
     ],
-	
+
     // 解决403错误
     ["meta", { name: "referrer", content: "no-referrer" }],
-	
     [
       "link",
       {
@@ -37,22 +36,63 @@ export default defineUserConfig({
       },
     ],
   ],
-
-
-  markdown: {
-    extractHeaders: {
-	  level: [1,3],
-	},
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "Zeblog",
+      description: "个人学习的笔记，记录学习过程遇到的问题，学到的知识，收集各种学习工具，各种技巧，各种使用教程。",
+    },
   },
-
-  hopeTheme,
-
+  theme,
+  shouldPrefetch: false,
   plugins: [
     docsearchPlugin({
       appId: "2T68NYRRBQ",
       apiKey: "37348b9c57bafc66de2383f8ed1b73ae",
       indexName: "zeml",
+      locales: {
+        "/": {
+          placeholder: "搜索",
+          translations: {
+            button: {
+              buttonText: "搜索",
+              buttonAriaLabel: "搜索",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
     }),
   ],
-  
 });
