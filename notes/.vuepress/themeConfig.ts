c912ac0a1eb44ab709867ac0f29d5bfbd0d2ee07
jiange1236 <183465355@qq.com>
2022-06-16 11:10:06 +0800

    navbar

--- notes/.vuepress/themeConfig.ts
+++ notes/.vuepress/themeConfig.ts
@@ -1,6 +1,4 @@
 import { hopeTheme } from "vuepress-theme-hope";
-import navbar from "./navbar";
-import sidebar from "./sidebar";
 
 export default hopeTheme({
   hostname: "https://jiange1236.github.io",
@@ -19,11 +17,38 @@ export default hopeTheme({
   docsDir: "notes",
   docsRepo: "https://github.com/jiange1236/jiange1236.github.io",
   docsBranch: "main",
+  
   // navbar
-  navbar: navbar,
+  navbar: [
+	  { text: '主页', link: '/' , icon: "house"},
+	  { text: '项目', link: '/home' ,icon:"list-check" },
+	  { text: '工作', link: '/work/' ,icon:"briefcase" },
+	  { text: '学习', link: '/study/' ,icon:"book-open"},
+	  { text: '计算机', link: '/tech/' ,icon:"desktop"},
+	  { text: '娱乐', link: '/fun/' ,icon:"dice"},  
+	  { text: '摄影', link: '/photo/' ,icon:"camera"},
+	  { text: '理财', link: '/finance/' ,icon:"chart-line"},
+	  { text: '其它', link: '/others/' ,icon:"bolt"},
+	  {
+		  text: "镜像",
+		  icon: "cube",
+		  children: [
+    		  { text: 'DownGit', link: 'https://github.jiange1236.workers.dev/' ,icon:"github"},
+			  { text: 'Proxy', link: 'https://proxy.jiange1236.workers.dev/' ,icon:"archway"},
+			  { 
+    			  text:'本站镜像',
+				  icon:"network",
+				  children: [
+    				  { text: 'Netflify', link: 'https://zeblog.netlify.app/' },
+					  { text: 'Vercel', link: 'http://zeblog.vercel.app/' },
+					  { text: 'Cloudflare', link: 'https://zeblog.pages.dev/' },
+				  ],
+			  },
+		  ],
+	  },
+  ],
 
   // sidebar
-  //sidebar: sidebar,
   sidebar: {
     "/finance/": "structure",
 	"/fun/": "structure",
