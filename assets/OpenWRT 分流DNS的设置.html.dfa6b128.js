8deeaed996f66c3344b31c4feb661f5139ef67cd
jiange1236 <183465355@qq.com>
2023-05-27 02:57:20 +0000

    Deploying to gh-pages from @ jiange1236/jiange1236.GitHub.io@a36254c1a10a51121beba0f3bc97a5d35e808f6d 🚀

---
+++ assets/OpenWRT 分流DNS的设置.html.dfa6b128.js
@@ -0,0 +1,97 @@
+import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";import{o as i,c as e,a as s}from"./app.093e0628.js";const d={},l=s(`<p>\u76EE\u524D\u8FD8\u662F\u6709\u8BB8\u591A\u4EBA\u559C\u6B22\u8BBE\u7F6EDNS\uFF0C\u867D\u7136\u4F1A\u6D6A\u8D39\u4E00\u4E9B\u8F6C\u53D1\u6027\u80FD\uFF0C\u4F46\u5982\u679C\u660E\u767D\u5176\u4E2D\u7684\u94FE\u8DEF\u5173\u7CFB\uFF0C\u5408\u7406\u8BBE\u7F6E\u540E\uFF0C\u8FD8\u662F\u80FD\u8D77\u5230\u4E0D\u9519\u7684\u6548\u679C\u7684\u3002\u540C\u65F6\u4E5F\u628A\u6B63\u786E\u7684\u8BBE\u7F6E\u65B9\u5F0F\u544A\u8BC9\u5927\u5BB6\u3002</p><p>\u6B64\u6559\u7A0B\u57FA\u4E8E\u5F53\u524D\u8DEF\u7531\u662F\u4E3B\u8DEF\u7531\uFF0C\u5982\u679C\u662F\u65C1\u8DEF\u7531\u867D\u5DEE\u4E0D\u591A\uFF0C\u4F46\u6709\u4E00\u4E9B\u5DEE\u5F02\uFF0C\u9700\u8981\u683C\u5916\u6CE8\u610F\u3002</p><p>\u540C\u65F6\u8BF7\u6CE8\u610F\uFF0C\u5220\u9664\u4F60\u7535\u8111\uFF0C\u624B\u673A\u4E0A\u7684DNS\uFF08\u4E0D\u8981\u8BBE\u7F6E\u4EFB\u4F55DNS\uFF09</p><h2 id="\u6CE8\u610F\u4E8B\u9879" tabindex="-1"><a class="header-anchor" href="#\u6CE8\u610F\u4E8B\u9879" aria-hidden="true">#</a> \u6CE8\u610F\u4E8B\u9879</h2><p>\u5EFA\u8BAE\u5C0F\u767D\u7528\u6237\u4F7F\u7528 Clash \u66F4\u7A33\u5B9A\u4E14\u65B9\u4FBF \u6B64\u6559\u7A0B\u4EC5\u9488\u5BF9\u6709\u7279\u6B8A\u9700\u6C42\u7684\u7528\u6237</p><h2 id="\u6D89\u53CA\u5230\u7684\u7EC4\u4EF6" tabindex="-1"><a class="header-anchor" href="#\u6D89\u53CA\u5230\u7684\u7EC4\u4EF6" aria-hidden="true">#</a> \u6D89\u53CA\u5230\u7684\u7EC4\u4EF6</h2><ul><li>Passwall</li><li>AdGuard Home\uFF08\u53EF\u9009\uFF09</li><li>Dnsmasq \uFF08\u65E0\u9700\u8FDB\u884C\u4EFB\u4F55\u8BBE\u7F6E\uFF09</li><li>SmartDNS \uFF08\u5206\u6D41+\u4E3BDNS\u670D\u52A1\u5668\uFF09</li></ul><h2 id="adguard-home-\u53EF\u9009" tabindex="-1"><a class="header-anchor" href="#adguard-home-\u53EF\u9009" aria-hidden="true">#</a> AdGuard Home\uFF08\u53EF\u9009\uFF09</h2><p>\u4E3B\u8981\u662F\u8D77\u5230\u8BB0\u5F55\u7684\u4F5C\u7528\uFF0C\u5BF9\u9700\u8981\u67E5\u770B\u8BB0\u5F55\u7684\u540C\u5B66\u6709\u5E2E\u52A9\uFF0C\u81F3\u4E8EDNS\u5E7F\u544A\u5C4F\u853D\uFF0C\u867D\u7136\u6709\u7528\u4F46\u4F1A\u8BEF\u4F24\uFF0C\u614E\u91CD\u9009\u62E9\u3002 \u540C\u65F6\u7531\u4E8E\u8BB0\u5F55\u6587\u4EF6\u8F83\u5927\uFF0C\u8BF7\u4E00\u5B9A\u8981\u6CE8\u610F<code>\u628A\u8BB0\u5F55\u65F6\u95F4\u6539\u5C0F\uFF0C\u6216\u8005\u8BBE\u7F6E\u5230\u6709\u8DB3\u591F\u7A7A\u95F4\u7684\u76EE\u5F55\u4E2D\uFF0C\u5426\u5219\u4F1A\u5BFC\u81F4\u65F6\u95F4\u4E45\u4E86\u5C06\u78C1\u76D8\u6491\u6EE1</code></p><ul><li>\u914D\u7F6E\u4E0A\u6E38DNS\u4E3A127.0.0.1</li><li>\u8BBE\u7F6E\u91CD\u5B9A\u541153\u7AEF\u53E3\u5230AdGuardHome\uFF0C\u5982\u679C\u4F7F\u7528\u4E86lean\u5927\u4F6C\u7684\u56FA\u4EF6\uFF0C\u8BF7\u5728\u9632\u706B\u5899\uFF0C\u81EA\u5B9A\u4E49\u89C4\u5219\u4E2D\u5220\u9664\u6DFB\u52A0\u7684\u4E24\u676153\u7AEF\u53E3\u91CD\u5B9A\u5411\u89C4\u5219\uFF08\u5927\u90E8\u5206\u4EBA\u90FD\u4F7F\u7528\u4E86lean\u7684\u56FA\u4EF6\uFF09</li></ul><p>\u6CE8\u610F\uFF1A\u8BBE\u7F6E\u4E86\u91CD\u5B9A\u5411\u540E\uFF0C\u6240\u6709DNS\u7ED3\u679C\u90FD\u5C06\u5E94\u7B54\u975E\u6743\u5A01\u8BB0\u5F55\uFF0C\u5982\u679C\u4E0D\u4E86\u89E3DNS\u534F\u8BAE\u8BF7\u65E0\u89C6\uFF0C\u8FD9\u6761\u5E76\u4E0D\u4F1A\u5F71\u54CD\u4EC0\u4E48\u3002</p><h2 id="smartdns" tabindex="-1"><a class="header-anchor" href="#smartdns" aria-hidden="true">#</a> SmartDNS</h2><p>\u4E3B\u8981\u7528\u4E8E\u5206\u6D41\u548CDNS\u67E5\u8BE2\uFF0C\u56FD\u5916\u8D70 Cloudflare DNS ove TLS/HTTPS \uFF0C\u56FD\u5185\u5EF6\u8FDF\u4F18\u5148\uFF08\u4F46\u53EF\u80FD\u5F71\u54CDP2P\u4E0B\u8F7D\uFF0C\u5B9E\u6D4B\u5B9E\u9645\u8FD8\u597D\uFF09\u3002</p><ul><li><p>\u542F\u7528\u7B2C\u4E00\u7EC4\u670D\u52A1\u5668\uFF0C\u9009\u62E9\u4F5C\u4E3Adnsmasq\u4E0A\u6E38\uFF0C\u5176\u4F59\u9ED8\u8BA4\u914D\u7F6E\u5373\u53EF\u3002</p></li><li><p>\u542F\u7528\u7B2C\u4E8C\u7EC4\u670D\u52A1\u5668\uFF0C\u7AEF\u53E37913\uFF0C\u8DF3\u8FC7\u6D4B\u901F\uFF0C\u8DF3\u8FC7Nameserver\u89C4\u5219\uFF08\u975E\u5E38\u91CD\u8981\uFF0C\u5426\u5219\u4F1A\u6B7B\u5FAA\u73AF\uFF09\uFF0C\u8DF3\u8FC7\u53CC\u6808\u4F18\u9009\uFF0C\u8DF3\u8FC7cache\uFF0C\u5176\u4F59\u8BBE\u7F6E\u4FDD\u6301\u9ED8\u8BA4\u3002</p></li><li><p>\u914D\u7F6E\u4E0A\u6E38\u670D\u52A1\u5668\uFF0C</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u90E8\u5206\u8BBE\u7F6E\u8981\u586B\u5199\u540E\u70B9\u7F16\u8F91\u624D\u80FD\u770B\u5230
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>\u3002</p><ul><li>\u56FD\u5185\u4E0A\u6E38\u586B\u5199\u4F60\u7684\u8FD0\u8425\u5546\uFF0C\u989D\u5916\u586B\u5199\u4E00\u4E9B\u56FD\u5185\u5E38\u7528\u7684\uFF0C\u4F8B\u5982\u963F\u91CC\u4E91\uFF0CDNSPod\u7B49\u7B49\uFF0C\u53EF\u4EE5\u53C2\u8003\u6587\u672B\u7684\u914D\u7F6E\uFF0C\u5206\u7EC4\u4FE1\u606F\u586B\u5199cn\u3002</li><li>\u56FD\u5916\u4E0A\u6E38\u586B\u51991.1.1.1\u7AEF\u53E3853\uFF0C\u534F\u8BAE\u586B\u5199tls\uFF0C\u5206\u7EC4\u4FE1\u606F\u586B\u5199passwall\uFF0C\u989D\u5916\u7684\u670D\u52A1\u53C2\u6570\u586B\u5199 -exclude-default-group</li></ul></li></ul><h2 id="passwall" tabindex="-1"><a class="header-anchor" href="#passwall" aria-hidden="true">#</a> Passwall</h2><ul><li>DNS\u5206\u6D41 smartdns</li><li>\u56FD\u5185\u5206\u7EC4\u540D cn</li><li>\u8FC7\u6EE4\u6A21\u5F0F \u901A\u8FC7UDP\u8BF7\u6C42DNS 127.0.0.1:7913</li></ul><h2 id="dnsmasq" tabindex="-1"><a class="header-anchor" href="#dnsmasq" aria-hidden="true">#</a> Dnsmasq</h2><p>\u65E0\u9700\u4EFB\u4F55\u8BBE\u7F6E\uFF0C\u5982\u679C\u6709\u8BBE\u7F6E\u8BF7\u6539\u56DE\uFF0C\u9664\u4E86\u4E0A\u6E38\u670D\u52A1\u5668\u662F127.0.0.1:6053\u3002</p><h2 id="\u4E00\u4E9B\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#\u4E00\u4E9B\u914D\u7F6E" aria-hidden="true">#</a> \u4E00\u4E9B\u914D\u7F6E</h2><p>smartdns \u90E8\u5206\u76F4\u63A5 vim \u7F16\u8F91 /etc/config/smartdns \u7167\u6284\u5373\u53EF\uFF0C\u65E0\u9700\u624B\u52A8\u8BBE\u7F6E\uFF0C\u914D\u7F6E\u5B8C\u8BB0\u5F97\u754C\u9762\u4E0A\u70B9\u51FB\u4FDD\u5B58\u5E94\u7528\uFF0C\u6216\u8005uci\u547D\u4EE4\u5237\u65B0\u914D\u7F6E\uFF0C\u6211\u91CC\u9762\u6709\u676D\u5DDE\u7535\u4FE1\u7684DNS\u670D\u52A1\u5668\uFF0C\u4E0D\u662F\u676D\u5DDE\u7684\u8BB0\u5F97\u81EA\u5DF1\u6539\u6389\uFF0C\u5426\u5219\u53EF\u80FD\u6709\u8D1F\u9762\u6548\u679C\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>config smartdns
+	option server_name &#39;smartdns&#39;
+	option port &#39;6053&#39;
+	option tcp_server &#39;1&#39;
+	option seconddns_tcp_server &#39;1&#39;
+	option coredump &#39;0&#39;
+	option seconddns_server_group &#39;passwall&#39;
+	option seconddns_no_speed_check &#39;1&#39;
+	option seconddns_no_dualstack_selection &#39;1&#39;
+	option prefetch_domain &#39;1&#39;
+	option ipv6_server &#39;0&#39;
+	option force_aaaa_soa &#39;1&#39;
+	option dualstack_ip_selection &#39;1&#39;
+	option serve_expired &#39;1&#39;
+	option redirect &#39;dnsmasq-upstream&#39;
+	option rr_ttl_min &#39;300&#39;
+	option seconddns_port &#39;7913&#39;
+	option seconddns_enabled &#39;1&#39;
+	option seconddns_no_rule_nameserver &#39;1&#39;
+	option seconddns_no_rule_addr &#39;0&#39;
+	option seconddns_no_rule_soa &#39;0&#39;
+	option seconddns_no_rule_ipset &#39;0&#39;
+	option cache_size &#39;300&#39;
+	option seconddns_no_cache &#39;1&#39;
+	option enabled &#39;1&#39;
+	list old_redirect &#39;dnsmasq-upstream&#39;
+	list old_port &#39;6053&#39;
+	list old_enabled &#39;1&#39;
+
+config server
+	option name &#39;aliyun&#39;
+	option ip &#39;223.5.5.5&#39;
+	option port &#39;53&#39;
+	option type &#39;udp&#39;
+	option blacklist_ip &#39;0&#39;
+	option server_group &#39;cn&#39;
+	option enabled &#39;1&#39;
+
+config server
+	option name &#39;114&#39;
+	option ip &#39;114.114.114.114&#39;
+	option port &#39;53&#39;
+	option type &#39;udp&#39;
+	option blacklist_ip &#39;0&#39;
+	option server_group &#39;cn&#39;
+	option enabled &#39;1&#39;
+
+config server
+	option enabled &#39;1&#39;
+	option type &#39;udp&#39;
+	option name &#39;\u7535\u4FE1&#39;
+	option ip &#39;202.101.172.35&#39;
+	option port &#39;53&#39;
+	option server_group &#39;cn&#39;
+	option blacklist_ip &#39;0&#39;
+
+config server
+	option enabled &#39;1&#39;
+	option type &#39;udp&#39;
+	option name &#39;\u7535\u4FE1&#39;
+	option ip &#39;202.101.172.47&#39;
+	option port &#39;53&#39;
+	option server_group &#39;cn&#39;
+	option blacklist_ip &#39;0&#39;
+
+config server
+	option type &#39;udp&#39;
+	option port &#39;53&#39;
+	option name &#39;DNSPod&#39;
+	option ip &#39;119.29.29.29&#39;
+	option blacklist_ip &#39;0&#39;
+	option server_group &#39;cn&#39;
+	option enabled &#39;1&#39;
+
+config server
+	option enabled &#39;1&#39;
+	option name &#39;cloud&#39;
+	option ip &#39;1.1.1.1&#39;
+	option port &#39;853&#39;
+	option type &#39;tls&#39;
+	option server_group &#39;passwall&#39;
+	option blacklist_ip &#39;0&#39;
+	option addition_arg &#39; -exclude-default-group&#39;
+
+config server
+	option enabled &#39;1&#39;
+	option type &#39;udp&#39;
+	option name &#39;CNNIC SDNS&#39;
+	option ip &#39;1.2.4.8&#39;
+	option port &#39;53&#39;
+	option server_group &#39;cn&#39;
+	option blacklist_ip &#39;0&#39;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u5982\u4F55\u9A8C\u8BC1" tabindex="-1"><a class="header-anchor" href="#\u5982\u4F55\u9A8C\u8BC1" aria-hidden="true">#</a> \u5982\u4F55\u9A8C\u8BC1\uFF1F</h2><p>\u767B\u5F55\u8DEF\u7531\u5668 \u4F7F\u7528 dig \u6216\u8005 nslookup \u68C0\u67E5\u4E0B\u5404\u7AEF\u53E3\u7684DNS\u4EE5\u53CA\u5206\u6D41\u60C5\u51B5</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>nslookup www.taobao.com 127.0.0.1:7913 \u8FD4\u56DE\u7684\u662F\u8282\u70B9\u5BF9\u5E94\u6DD8\u5B9D\u6700\u5FEB\u7684IP
+nslookup www.taobao.com 127.0.0.1:6053 \u8FD4\u56DE\u7684\u662F\u56FD\u5185\u6700\u5FEB\u7684IP
+nslookup www.taobao.com \u5E94\u8BE5\u662F\u56FD\u5185
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>\u6CE8\uFF1A\u5982\u679C\u624B\u52A8\u67E5\u8BE2\u89C4\u5219\u5217\u8868\u5185\u7684\u57DF\u540D\uFF0C\u4F7F\u7528\u7AEF\u53E36053\uFF0C\u7136\u540E\u5339\u914D\u89C4\u5219\uFF0C\u8F6C\u53D1\u7ED97913\uFF0C\u7136\u540E\u88AB\u7F13\u5B58\u4F4F\u3002\uFF08\u56FD\u5916\u56E0\u4E3A\u8DF3\u8FC7\u6D4B\u901F\uFF0C\u6240\u4EE5\u591A\u4E2A\u57DF\u540D\u662F\u6B63\u786E\u7684\uFF09</p>`,25),a=[l];function t(o,r){return i(),e("div",null,a)}var p=n(d,[["render",t],["__file","OpenWRT \u5206\u6D41DNS\u7684\u8BBE\u7F6E.html.vue"]]);export{p as default};
